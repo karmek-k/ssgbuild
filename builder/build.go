c4d7e660d1a0010a5719df0b70f2a5cf0878de57
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-02-21 15:54:21 +0100

    Move CheckDir to the utils package

--- builder/build.go
+++ builder/build.go
@@ -29,7 +29,7 @@ func Build(cfg *BuildConfig, log *zap.SugaredLogger) error {
 	log.Debugw("changing to the base directory",
 		"dir", cfg.BaseDir,
 	)
-	if err := CheckDir(cfg.BaseDir); err != nil {
+	if err := utils.CheckDir(cfg.BaseDir); err != nil {
 		log.Errorw("checking base dir failed",
 			"name", cfg.Name,
 			"dir", cfg.BaseDir,
@@ -80,7 +80,7 @@ func Build(cfg *BuildConfig, log *zap.SugaredLogger) error {
 	log.Debugw("checking the result dir",
 		"dir", cfg.ResultDir,
 	)
-	if err := CheckDir(cfg.ResultDir); err != nil {
+	if err := utils.CheckDir(cfg.ResultDir); err != nil {
 		log.Errorw("checking result dir failed",
 			"name", cfg.Name,
 			"dir", cfg.ResultDir,
